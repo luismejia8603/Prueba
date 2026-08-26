dcb2c9f9f9c62394cd4a38a823e017c777a4c9d0
Luis Mejía <luisfmn.lfmd@gmail.com>
2026-08-25 19:46:44 -0600

    Funciones repaso

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -4,37 +4,58 @@ namespace Prueba
 {
     internal class Program
     {
+        static double CalcularPromedio (double numero1, double numero2, double numero3) 
+        { 
+            double total = (numero1 + numero2 + numero3)/3;
+
+
+            return total;
+        }
+
+        static bool Aprobado(double promedio)
+        {
+            bool aprobado = false;
+
+            if (promedio >= 6)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+
+        }
+
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingresar número 1: ");
+            double nota1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Ingresar una palabra: ");
-            string palabra = Console.ReadLine().Trim();
+  
+            Console.WriteLine("Ingresar nota 2: ");
+            double nota2 = double.Parse(Console.ReadLine());
 
-            Console.WriteLine(palabra.ToUpper());
+            Console.WriteLine("Ingresar nota 3: ");
+            double nota3 = double.Parse(Console.ReadLine());
 
-            string invertida = "";
 
-            Console.WriteLine($"Original: {palabra}");
-            
-            for (int i = palabra.Length-1; i >= 0; i--)
+
+            double total = CalcularPromedio(nota1, nota2,nota3);
+
+            Console.WriteLine($"Nota promedio es: {total:F2}");
+
+            bool estado = Aprobado(total);
+
+            if (estado == true)
             {
-                invertida += palabra[i];
-
-                
+                Console.WriteLine("El estudiante aprobó :)");
             }
-
-            Console.WriteLine($"Invertida: {invertida}");
-
-            if (palabra == invertida)
+            else 
             {
-                Console.WriteLine("La palabra es palíndromo.");
+                Console.WriteLine("El estudiante reprobó :(");
             }
-            else
-            {
-                Console.WriteLine("La palabra no es palíndromo.");
-            }
-
-
 
 
             Console.ReadKey();
